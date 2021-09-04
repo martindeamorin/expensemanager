f94d5c31e86acf5514d0c7ce73193be27513a330
Martín de Amorín <martindeamorin@gmail.com>
2021-09-04 16:24:32 -0300

    middleware security update (positive number, concept alpha)

--- api/middlewares/operationMiddleware.js
+++ api/middlewares/operationMiddleware.js
@@ -3,8 +3,10 @@ const {body} = require("express-validator");
 const operationMiddleware = {
     operationValidation : [
         body("type", "Debe seleccionar el tipo de operación.").notEmpty().toBoolean().isBoolean(),
-        body("amount", "Debe seleccionar una cantidad.").notEmpty().isNumeric(),
-        body("concept", "Debe especificar un concepto.").notEmpty(),
+        body("amount", "Debe seleccionar una cantidad (valor positivo)").notEmpty().isNumeric().custom((value) => {
+            return value > 0;
+        }),
+        body("concept", "Debe especificar un concepto (solo letras)").notEmpty().matches(new RegExp(/^[a-zA-Z\s]*$/)),
         body("date", "Debe ingresar una fecha").notEmpty().isDate()
     ]
 }
